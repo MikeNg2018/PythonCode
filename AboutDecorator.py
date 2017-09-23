6ae411e7e9d603a64666b577b45871de0b165b2b
MikeNg2018 <mikeng83@gmail.com>
2017-09-23 16:06:53 +0800

    test

---
+++ AboutDecorator.py
@@ -0,0 +1,22 @@
+def use_logging(func):
+    def wrapper(*args,**kwargs):
+        print("%s is running" % func.__name__)
+        print('wrapper')
+        print("name=",args[0])
+        print("age=",args[1])
+        return func(*args,**kwargs)   # 把 foo 当做参数传递进来时，执行func()就相当于执行foo()
+    return wrapper
+
+@use_logging
+def foo(*args,**kwargs):
+    print("i am %s , age %s" % (args[0],args[1]))
+    print("He say: %s" % kwargs['a'])
+
+@use_logging
+def test():
+    print('Test it!')
+#foo = use_logging(foo)  # 因为装饰器 use_logging(foo) 返回的时函数对象 wrapper，这条语句相当于  foo = wrapper
+
+foo("Mike","20",a='hello',b=3,c='C')
+
+#test()
