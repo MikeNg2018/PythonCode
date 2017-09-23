f185ed98508d7967c58a775d9b0c6df5decec8fa
MikeNg2018 <mikeng83@gmail.com>
2017-09-23 17:32:14 +0800

    test2

--- AboutDecorator.py
+++ AboutDecorator.py
@@ -7,16 +7,30 @@ def use_logging(func):
         return func(*args,**kwargs)   # 把 foo 当做参数传递进来时，执行func()就相当于执行foo()
     return wrapper
 
+def use_logging2(level):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if level == "warn":
+                print("%s is running warn" % func.__name__)
+            elif level == "info":
+                print("%s is running info" % func.__name__)
+            return func(*args)
+        return wrapper
+    return decorator
+
 @use_logging
 def foo(*args,**kwargs):
     print("i am %s , age %s" % (args[0],args[1]))
     print("He say: %s" % kwargs['a'])
 
-@use_logging
+@use_logging2(level='info')
 def test():
     print('Test it!')
 #foo = use_logging(foo)  # 因为装饰器 use_logging(foo) 返回的时函数对象 wrapper，这条语句相当于  foo = wrapper
 
-foo("Mike","20",a='hello',b=3,c='C')
+foo("Mike","30",a='hello',b=3,c='C')
 
-#test()
+test()
+
+
+
