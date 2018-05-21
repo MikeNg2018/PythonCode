420166f45bc2b0e5b6bfa8097d2b79e8f130bf80
MikeNg2018 <mikeng83@gmail.com>
2018-05-21 17:54:19 +0800

    Django test

--- Django/zqxt_tmpl/learn/views.py
+++ Django/zqxt_tmpl/learn/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 
+# def home(request):
+#     string = "测试Django"
+#     return render(request, 'learn/home.html', {'string': string})
+
 def home(request):
-    return render(request, 'home.html')
+    TutorialList = ['HTML', "CSS", "Python", "Django"]
+    return render(request, 'learn/home.html', {'TutorialList': TutorialList})
+
 
